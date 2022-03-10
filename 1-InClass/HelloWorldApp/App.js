43e67ed6dbbaf9262678c9c6857bcd73753fb8c8
Mark Fulton <mark@9rockets.com>
2022-03-10 00:29:35 -0600

    init

---
+++ 1-InClass/HelloWorldApp/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, My Name is Mark</Text>
+      <Image source={{
+            uri: 'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+          }}
+          style={{ width: 400, height: 300 }}
+          />
+      <Text>I am a web developer by day.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
